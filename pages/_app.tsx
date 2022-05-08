0793d4d0f6cae4acb2ee2d956e82f87ec9f886eb
Alex-Ostrovny <alexandr.ostrovny@wesoftyou.com>
2022-05-08 17:54:31 +0300

    Add Htag component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My top is the best top</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Noto+Sans:wght@300;400;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp;
