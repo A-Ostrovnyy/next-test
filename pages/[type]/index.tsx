9b787976d86244c07f12ae744d2562bee1960268
Alex-Ostrovny <sanny1603@gmail.com>
2022-08-03 10:54:11 +0300

    Add search and type pages

---
+++ pages/[type]/index.tsx
@@ -0,0 +1,47 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import axios from 'axios';
+import { ParsedUrlQuery } from 'node:querystring';
+
+import { withLayout } from '../../layout/Layout';
+import { MenuItem } from '../../interfaces/menu.interface';
+import { firstLevelMenu } from '../../helpers/helpers';
+
+
+function Type({ firstCategory }: TypeProps): JSX.Element {
+    return (
+        <>
+            Search page: {firstCategory}
+        </>
+    );
+}
+
+export default withLayout(Type);
+
+export const getStaticProps: GetStaticProps<TypeProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
+    const firstCategoryItem = firstLevelMenu.find(({ route }) => route === params?.type);
+    if (!params || !firstCategoryItem) {
+        return {
+            notFound: true
+        }
+    }
+    const { data: menu } = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`,
+        { firstCategory: firstCategoryItem.id });
+    return {
+        props: {
+            firstCategory: firstCategoryItem.id,
+            menu
+        }
+    };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: firstLevelMenu.map(({ route }) => `/${route}`),
+        fallback: true
+    }
+}
+
+interface TypeProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
---
+++ pages/search.tsx
@@ -0,0 +1,32 @@
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+
+import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
+
+
+function Search(): JSX.Element {
+    return (
+        <>
+            Search page
+        </>
+    );
+}
+
+export default withLayout(Search);
+
+export const getStaticProps: GetStaticProps<SearchProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`, { firstCategory });
+    return {
+        props: {
+            firstCategory,
+            menu
+        }
+    };
+};
+
+interface SearchProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
