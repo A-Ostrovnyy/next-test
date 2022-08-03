635906df8d39de4f96b0f4b194fec05d6212c2ff
Alex-Ostrovny <sanny1603@gmail.com>
2022-08-03 11:39:59 +0300

    Add sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,8 +1,18 @@
 import { FC } from 'react';
+import cn from 'classnames';
+
+import { Menu } from '../Menu/Menu';
+import Logo from '../logo.svg';
 
 import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 
-export const Sidebar: FC<SidebarProps> = ({...props}) => {
-    return <div {...props}>Sidebar</div>
+export const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
+    return (
+        <div className={cn(className, styles.sidebar)} {...props}>
+            <Logo className={styles.logo} />
+            <div>Search</div>
+            <Menu />
+        </div>
+    )
 }
