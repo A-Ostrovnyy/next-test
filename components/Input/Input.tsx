7d1eec455657df9732f81da89a994072108a84aa
Alex-Ostrovny <sanny1603@gmail.com>
2022-09-01 19:48:08 +0300

    Fix input

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import cn from 'classnames';
 
 import { InputProps } from './Input.props';
-import styles from './P.module.css';
+import styles from './Input.module.css';
 
 export const Input: FC<InputProps> = ({ className, ...props }) => {
     return <input className={cn(styles.input, className)} {...props} />
